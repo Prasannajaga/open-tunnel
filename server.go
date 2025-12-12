cb6aed7ac9d2374938e6f1b9e1161cf47733d5a1
prasanna <prasannajaga9@gmail.com>
2025-12-12 18:05:11 +0530

    server modification and client added

--- server.go
+++ server.go
@@ -7,36 +7,56 @@ import (
 )
 
 func main() {
-
-	clientListener, err := net.Listen("tcp", ":9000")
+	// Control channel: client connects here
+	ctrlListener, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Waiting for client on :9000...")
+	log.Println("Waiting for client control connection on :9000...")
 
-	clientConn, err := clientListener.Accept()
+	ctrlConn, err := ctrlListener.Accept()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Client connected")
+	log.Println("Client connected on control channel")
 
-	// Accept external users
-	publicListener, err := net.Listen("tcp", ":9001")
+	// Public listener
+	pubListener, err := net.Listen("tcp", ":9001")
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Public listener on :9001")
 
 	for {
-		externalConn, err := publicListener.Accept()
+		externalConn, err := pubListener.Accept()
 		if err != nil {
-			log.Println("Public accept error:", err)
+			log.Println("Error accepting external:", err)
 			continue
 		}
 		log.Println("External user connected")
 
-		// Forward external <-> client
-		go io.Copy(clientConn, externalConn)
-		go io.Copy(externalConn, clientConn)
+		// Request a new data connection from the client
+		_, err = ctrlConn.Write([]byte("new\n"))
+		if err != nil {
+			log.Println("Control write failed:", err)
+			externalConn.Close()
+			continue
+		}
+
+		// The client will call net.Dial back to the server on :9002
+		dataConn, err := ctrlListener.Accept()
+		if err != nil {
+			log.Println("Data connection failed:", err)
+			externalConn.Close()
+			continue
+		}
+
+		go func() {
+			defer dataConn.Close()
+			defer externalConn.Close()
+
+			go io.Copy(dataConn, externalConn)
+			io.Copy(externalConn, dataConn)
+		}()
 	}
 }
